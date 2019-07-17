751f993f5edcd68bd94b27c811574638caaa1787
mls-kll <milos.kallai@jaystack.com>
2019-07-17 17:28:15 +0200

    app uses withapollo

--- apollo-react-example/src/App.js
+++ apollo-react-example/src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Posts from './components/Posts';
 import './App.css';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, withApollo } from 'react-apollo';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/'
 });
 
+const PostsWithClient = withApollo(Posts);
+
 const App = () => (
   <ApolloProvider client={client}>
     <div className="App">
       <h1>Fake blog posts</h1>
-      <Posts />
+      <PostsWithClient />
     </div>
   </ApolloProvider>
 );
--- apollo-react-example/src/components/Posts.js
+++ apollo-react-example/src/components/Posts.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import Post from './Post';
-import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
 
-const Posts = ({ posts }) => (
-  <Query
-    query={gql`
-      {
-        posts {
-          id
-          title
-          body
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error</p>;
-      return data.posts.map(({ id, title, body }) => (
-        <Post key={id} title={title} body={body} />
-      ));
-    }}
-  </Query>
-);
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts = () => {
+    this.props.client
+      .query({
+        query: gql`
+          {
+            posts {
+              id
+              title
+              body
+            }
+          }
+        `
+      })
+      .then(result => {
+        this.setState({ posts: result.data.posts });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Posts;
