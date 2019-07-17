32ecd46866f8dd403fa1f2051b4a6d92b7cf6c75
mls-kll <milos.kallai@jaystack.com>
2019-07-17 13:06:52 +0200

    apollo client connected to react, useing mock data

--- apollo-react-example/src/App.js
+++ apollo-react-example/src/App.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import Posts from './components/Posts';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
-  }
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/'
+});
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data =>
-        this.setState({
-          posts: data
-        })
-      );
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Read posts in fake latin</h1>
-        <Posts posts={this.state.posts} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <h1>Lorem ipsum posts</h1>
+      <Posts />
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
--- apollo-react-example/src/components/Posts.js
+++ apollo-react-example/src/components/Posts.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Post from './Post';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
 
-const Posts = ({ posts }) => {
-  return (
-    <div className='post-container'>
-      {posts.map(post => (
-        <Post title={post.title} body={post.body} />
-      ))}
-    </div>
-  );
-};
+const Posts = ({ posts }) => (
+  <Query
+    query={gql`
+      {
+        posts {
+          id
+          title
+          body
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error</p>;
+      return data.posts.map(({ id, title, body }) => (
+        <Post key={id} title={title} body={body} />
+      ));
+    }}
+  </Query>
+);
 
 export default Posts;
